5646fd97fb9d1888113cf5e7c750153294217da8
Kevin Liu <liu.jianq@husky.neu.edu>
2018-04-11 12:39:16 -0400

    register class model

--- MFWWeb/Dashboard/views.py
+++ MFWWeb/Dashboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Student, Teacher
+from .models import Student, Teacher, Class
 from django.views import generic
 from .forms import Quiz
 from django.http import HttpResponseRedirect
